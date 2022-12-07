bb613db2329a188a9469e0ba185f7fe6af744fd2
MarkZakelj <markzakelj@gmail.com>
2022-12-07 18:29:24 +0100

    openapi docs code

--- src/main/java/team/globaloptima/DeliveryPersonResource.java
+++ src/main/java/team/globaloptima/DeliveryPersonResource.java
@@ -1,5 +1,12 @@
 package team.globaloptima;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -16,12 +23,23 @@ public class DeliveryPersonResource {
     DeliveryPersonService deliveryPersonBean;
 
     @GET
+    @Operation(summary = "Get a list of delivery people", description = "Returns a list of all delivery people.")
+    @APIResponses({
+            @APIResponse(description = "List of delivery people", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = DeliveryPerson.class,
+                            type = SchemaType.ARRAY)))
+    })
     public Response getAllDeliveryPeople() {
         List<DeliveryPerson> deliveryPeople = deliveryPersonBean.getDeliveryPeople();
         return Response.ok(deliveryPeople).build();
     }
 
     @GET
+    @Operation(summary = "Get delivery person details", description = "Return details of one delivery person with selected id")
+    @APIResponses({
+            @APIResponse(description = "Delivery Person", responseCode = "200",
+                    content = @Content(schema = @Schema(implementation = DeliveryPerson.class)))
+    })
     @Path("{deliveryPersonId}")
     public Response getDeliveryPerson(@PathParam("deliveryPersonId") Integer deliveryPersonId) {
         DeliveryPerson deliveryPerson = deliveryPersonBean.getDeliveryPerson(deliveryPersonId);
@@ -31,12 +49,25 @@ public class DeliveryPersonResource {
     }
 
     @POST
+    @Operation(summary = "Create new Delivery person", description = "Create new delivery person")
+    @APIResponses({
+            @APIResponse(responseCode = "204",
+                    description = "New customer created"
+            )
+    })
     public Response addNewDeliveryPerson(DeliveryPerson deliveryPerson) {
         deliveryPersonBean.addDeliveryPerson(deliveryPerson);
         return Response.noContent().build();
     }
 
     @DELETE
+    @Operation(description = "Delete delivery person", summary = "Delete delivery person")
+    @APIResponses({
+            @APIResponse(
+                    responseCode = "204",
+                    description = "Delivery person successfully deleted."
+            )
+    })
     @Path("{deliveryPersonId}")
     public Response deleteDeliveryPerson(@PathParam("deliveryPersonId") Integer deliveryPersonId) {
         deliveryPersonBean.deleteDeliveryPerson(deliveryPersonId);
